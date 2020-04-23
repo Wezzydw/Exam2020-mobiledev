f53e7ce54fe505cdd20e3c9cd3631c8c474dcb4a
Wezzydw <44640169+Wezzydw@users.noreply.github.com>
2020-04-23 14:14:28 +0200

    Naming convention

--- app/src/main/java/com/example/exam2020_certificateapp/UserSettingsActivity.java
+++ app/src/main/java/com/example/exam2020_certificateapp/UserSettingsActivity.java
@@ -22,6 +22,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -30,34 +31,35 @@ public class UserSettingsActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_IMAGE_UPLOAD = 2;
-    FloatingActionButton btnTakePicture;
-    FloatingActionButton btnGetGalleryPicture;
-    Button btnDeleteUser;
-    ImageView imageViewProfilePicture;
-    ImageButton btnGoBack;
-    EditText editTextName;
-    int  MY_PERMISSIONS_REQUEST_CAMERA;
-    int  MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE;
-    String name = "";
+    FloatingActionButton mBtnTakePicture;
+    FloatingActionButton mBtnGetGalleryPicture;
+    Button mBtnDeleteUser;
+    ImageView mImageViewProfilePicture;
+    ImageButton mBtnGoBack;
+    EditText mEditTextName;
+    int MY_PERMISSIONS_REQUEST_CAMERA;
+    int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE;
+    String mName = "";
+    FirebaseFirestore mDb = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
 
-        btnTakePicture = findViewById(R.id.settingsBtnTakePicture);
-        btnGetGalleryPicture = findViewById(R.id.settingsBtnUploadPicture);
-        imageViewProfilePicture = findViewById(R.id.settingsImageView);
-        btnGoBack = findViewById(R.id.settingsBtnReturn);
-        btnDeleteUser = findViewById(R.id.settingsBtnDeleteUser);
-        editTextName = findViewById(R.id.settingsInputName);
+        mBtnTakePicture = findViewById(R.id.settingsBtnTakePicture);
+        mBtnGetGalleryPicture = findViewById(R.id.settingsBtnUploadPicture);
+        mImageViewProfilePicture = findViewById(R.id.settingsImageView);
+        mBtnGoBack = findViewById(R.id.settingsBtnReturn);
+        mBtnDeleteUser = findViewById(R.id.settingsBtnDeleteUser);
+        mEditTextName = findViewById(R.id.settingsInputName);
 
 
         View.OnClickListener buttons = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int id = v.getId();
-                switch (id){
+                switch (id) {
                     case R.id.settingsBtnReturn:
                         returnToActivity();
                         break;
@@ -74,23 +76,20 @@ public class UserSettingsActivity extends AppCompatActivity {
             }
         };
 
-        btnTakePicture.setOnClickListener(buttons);
-        btnGoBack.setOnClickListener(buttons);
-        btnGetGalleryPicture.setOnClickListener(buttons);
-        btnDeleteUser.setOnClickListener(buttons);
+        mBtnTakePicture.setOnClickListener(buttons);
+        mBtnGoBack.setOnClickListener(buttons);
+        mBtnGetGalleryPicture.setOnClickListener(buttons);
+        mBtnDeleteUser.setOnClickListener(buttons);
 
     }
 
     void openCamera() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
-        {
-           //Ask permission
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
-        }
-        else {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            //Ask permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+        } else {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if(takePictureIntent.resolveActivity(getPackageManager()) != null)
-            {
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
@@ -98,36 +97,32 @@ public class UserSettingsActivity extends AppCompatActivity {
 
     void uploadImage() {
 
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-        {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             //Ask permission
-            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-        }
-        else {
-                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(galleryIntent, REQUEST_IMAGE_UPLOAD);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        } else {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, REQUEST_IMAGE_UPLOAD);
         }
     }
 
 
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageViewProfilePicture.setImageBitmap(imageBitmap);
+
+            mImageViewProfilePicture.setImageBitmap(imageBitmap);
 
             //Upload to firebase here
-        }
-        else if (requestCode == REQUEST_IMAGE_UPLOAD && resultCode == RESULT_OK) {
+        } else if (requestCode == REQUEST_IMAGE_UPLOAD && resultCode == RESULT_OK) {
 
             Uri uri = data.getData();
             try {
                 InputStream imageStream = getContentResolver().openInputStream(uri);
                 Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
-                imageViewProfilePicture.setImageBitmap(bitmap);
+                mImageViewProfilePicture.setImageBitmap(bitmap);
                 //upload bitmap
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -137,7 +132,7 @@ public class UserSettingsActivity extends AppCompatActivity {
     }
 
     void returnToActivity() {
-promtForSaveSettings();
+        promtForSaveSettings();
         //Save all data changes to firebase
 
 
@@ -165,20 +160,18 @@ promtForSaveSettings();
     }
 
     void fireBaseDeleteAccount() {
-        editTextName.setText("Virker");
+        //editTextName.setText("Virker");
         //Delete user from firebase here
     }
 
 
-    void saveSettings()
-    {
-        editTextName.setText("Changes Saved");
+    void saveSettings() {
+        //editTextName.setText("Changes Saved");
     }
 
     void promtForSaveSettings() {
         //editTextName.setText("Checking save");
-        if(!name.equals(editTextName.getText().toString()))
-        {
+        if (!mName.equals(mEditTextName.getText().toString())) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Savesettings");
             builder.setMessage("Save changes?");
@@ -199,8 +192,7 @@ promtForSaveSettings();
             });
             AlertDialog alert = builder.create();
             alert.show();
-        }
-        else {
+        } else {
             finish();
         }
     }
